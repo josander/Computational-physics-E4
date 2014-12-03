a8c6cb60f2d7e5aaaa8f84249d758eda150bf164
Josephine <jossan_cuellar@hotmail.com>
2014-12-03 22:03:53 +0100

    It works

--- E4.c
+++ E4.c
@@ -7,45 +7,77 @@
 #include <stdlib.h>
 #include "E4_func.h"
 #define PI 3.141592653589
+# define kB 0.000086173324
 
 /* Main program */
 int main()
 {
+
+	// Declaration of variables
+	int i, j, k;
+	double ny;
+	double omega;
+	double c0;
+	double m;
+	double temp;
+	double dt;
+	double timesteps;
+	double g1, g2;
+
+	// Initiation of variables
+	ny = 1;
+	omega = 1;
+	c0 = exp(-ny * dt);
+	m = 1;
+	temp = 300;
+	dt = 0.1;
+	timesteps = 1000;
+	
+	// Declaration of arrays
+	double *x = malloc(timesteps * sizeof(double));
+	double *v = malloc(timesteps * sizeof(double));
+	double *a = malloc(timesteps * sizeof(double));
+
+	// Initiation of arrays
+	for(j = 0; j < timesteps; j++){
+		x[j] = 0.0;
+		v[j] = 0.0;
+		a[j] = 0.0;
+	}
+
 	// Seed for generating random numbers
 	srand(time(NULL));
 
-	// Task 1: calculate the integral for different N
-	integral_uniform();
+	// Time evolution
+	for(i = 0; i < timesteps; i++){
 
-	// Task 2: calculate the integral for different N using importance sampling
-	integral_sine();
-
-	// Task 3: calculate the integral for different N using the Metropolis algorithm
-	integral_metropolis();
-
-	// Task 4: Evaluate the statistical inefficiency
-	// Get data from file MC.txt and save it in 'data'
-	FILE *fr;
-	fr = fopen("MC.txt","r");
+		// Generate two random numbers with gaussian distribution
+		g1 = (double) rand() / RAND_MAX;
+		g2 = (double) rand() / RAND_MAX;		
 	
-	int N = 1000000;
-    	float data[N];
-    	int i;
+		// v(t+)
+		v[i] = sqrt(c0)*v[i] + sqrt(kB * temp / m) * sqrt(1-c0)*g1;
 	
-    	for (i = 0; i < N; i++){
-		data[i] = 0.0;
-        	fscanf(fr, "%f\n", &data[i]);
-    	}
-	fclose(fr);
+		// v(t + dt/2)
+		v[i] = v[i] + a[i] * dt / 2;
 	
-	// Call the error-method to calculate the statistical inefficiency
-	printf("STATISTICAL INEFFICIENCY FROM CORR FUNC\n");	
-	error_corr_func(data, N);
+		// x(t + dt)
+		x[i] = x[i] + v[i] * dt;
 
-	// Call the error-method to calculate the statistical inefficiency
-	printf("STATISTICAL INEFFICIENCY FROM BLOCK AVERAGE\n");	
-	error_block_average(data, N);
+		// Calc new acceleration
+		a[i] = acceleration(v[i], x[i], ny, omega);
 
-	printf("Done! \n");
+		// v(t- + dt)
+		v[i] = v[i] + a[i] * dt / 2;
+
+		// v(t + dt)
+		v[i] = sqrt(c0) * v[i] + sqrt(kB * temp / m) * sqrt(1-c0)*g2;
+
+	}
+
+	// Free allocated memory
+	free(x); free(v); free(a);
+	x = NULL; v = NULL; a = NULL;
+
 }
 
--- E4_func.h
+++ E4_func.h
@@ -5,10 +5,6 @@ E4_func.h
 #ifndef _E4_func_h
 #define _E4_func_h
 
-extern void integral_uniform();
-extern void integral_sine();
-extern void integral_metropolis();
-extern void error_corr_func(float *, int);
-extern void error_block_average(float *, int);
+extern double acceleration(double, double, double, double);
 
 #endif
